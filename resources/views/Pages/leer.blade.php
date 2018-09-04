cf4edd19b0170a095f65a55e5b0343b6550df027
anibalgOM <anibalg@osmondmarketing.com>
2018-09-03 22:40:21 -0600

    Delete Noticia
    Los usuarios registrados pueden eliminar noticias

--- resources/views/Pages/leer.blade.php
+++ resources/views/Pages/leer.blade.php
@@ -9,6 +9,13 @@
     <div class="container">
         <p style="font-size:40px; font-weight: bold;">{{ $noticia->title }}</p>
         <p><img src="{{ asset("storage/" . $noticia->image)}}" /></p>
-            <p>{!!html_entity_decode($noticia->body)!!}</p>
+        <p>{!!html_entity_decode($noticia->body)!!}</p>
+        @if (Auth::check())
+        <form action="{{ route('del_noticia', ['id' => $id]) }}" method="get">
+            {!! csrf_field() !!}
+            <input type="submit" value="Eliminar">
+        </form>
+        <p><a href="">Editar</a></p>
+        @endif
     </div>
-@endsection
+    @endsection
